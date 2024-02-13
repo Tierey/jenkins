0db5a684b5533e0eaf71a7678096024e91303d01
Tierey <16826506+Tierey@users.noreply.github.com>
2024-02-14 02:15:54 +0300

    Update job1.groovy

--- devops/dsl/job1.groovy
+++ devops/dsl/job1.groovy
@@ -1,1 +1,21 @@
+#!grrovy
+pipelineJob('pipelineJob-dsl') {
+  parameters {
+      booleanParam('FLAG', true)
+      choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
+  }
 
+  definition {
+      cpsScm {
+          scm {
+              
+              git{
+                remote{
+                  url('https://github.com/Tierey/jenkins.git')
+                }
+                branch('master')
+              }
+          }
+      }
+  }
+}
