0fa85822b5dc9b6c5228741396bba51ad9fad2ee
Tierey <16826506+Tierey@users.noreply.github.com>
2024-02-14 03:42:25 +0300

    Update init.sh

--- devops/init.sh
+++ devops/init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-echo "helo world"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "helo world from $SCRIPT_DIR"
 mkdir -p $CASC_JENKINS_CONFIG/Tierey
 cp -rf $(pwd)/devops/jcasc/Tierey $CASC_JENKINS_CONFIG
 jcli reload-jcasc-configuration
 
 echo "update-plugins"
 
-UPDATE_LIST=$( jcli list-plugins | grep -e ')$' | awk '{ print $1 }' ); 
+UPDATE_LIST=$( jcli list-plugins | grep -e ')$' | awk '{ print $1 }' );
+UPDATE_LIST=""; 
 echo "plugins : $UPDATE_LIST"
 if [ ! -z "${UPDATE_LIST}" ]; then 
     echo Updating Jenkins Plugins: ${UPDATE_LIST}; 
