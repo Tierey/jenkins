e8395bb4acf7e06f0b162cab4f9bd61ce11988b0
Tierey <16826506+Tierey@users.noreply.github.com>
2024-02-14 03:17:50 +0300

    Update init.sh

--- devops/init.sh
+++ devops/init.sh
@@ -3,5 +3,7 @@
 echo "helo world"
 mkdir -p $CASC_JENKINS_CONFIG/Tierey
 cp -rf $(pwd)/devops/jcasc/Tierey $CASC_JENKINS_CONFIG
+jcli who-am-i
+
 ls -la $CASC_JENKINS_CONFIG/Tierey
 echo "bye world"
